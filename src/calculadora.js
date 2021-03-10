7711ebc786adfbe16c4ceea9955f7aac10e5cf4e
Fabio Melo <fabiocmelo@gmail.com>
2021-03-09 21:48:04 -0300

    primeiras modificações

--- src/calculadora.js
+++ src/calculadora.js
@@ -5,16 +5,28 @@
  *  não pode começar com um operador, exceto o de subtração
  */
 function enterNumer(element) {
-    const valorNovo = element.innerText;
-    const valorAntigo = document.getElementById('entrada').value;
+    const entrada = element.innerText;
+    const display = document.getElementById('entrada').value;
+    const entradaValida = validaEntrada(entrada, display)
 
     //é um sinal, e se o ultimo caracter tbm
-    // condicionais
-    document.getElementById('entrada').value = valorAntigo + valorNovo;
 
+    // condicionais
+    if (entradaValida) {
+        document.getElementById('entrada').value = display + entrada;    
+        
+    }
 }
 
+    function validaEntrada(valorEntrada, valorDisplay) {
+        let entradaValida = true
+        
+        if ((valorEntrada === '+' || valorEntrada === '*' || valorEntrada === "/") && valorDisplay === '') {
+            entradaValida = false
+        }
 
+        return entradaValida
+    }
 
 /**
  * implementar a função "result" para efetuar o calculo
