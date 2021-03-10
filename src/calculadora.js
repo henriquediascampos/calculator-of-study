7a02aebfbc681a7a126d3bef25cdca9309e9bb5d
Henrique Dias Campos <henrique.campos@mv.com.br>
2021-03-09 21:43:57 -0300

    primeira validação, envitar caracter operador no inicio

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,18 +2,38 @@
 /**
  * - validar as entradas
  *  não permitir que tenha dois operadores seguido do outro
- *  não pode começar com um operador, exceto o de subtração
+ *  não pode começar com um operador,
+ *  - exceto o de subtração
  */
 function enterNumer(element) {
-    const valorNovo = element.innerText;
-    const valorAntigo = document.getElementById('entrada').value;
+    const entrada = element.innerText;
+    const display = document.getElementById('entrada').value;
+    const entradaValida = validaEntrada(entrada, display);
 
     //é um sinal, e se o ultimo caracter tbm
     // condicionais
-    document.getElementById('entrada').value = valorAntigo + valorNovo;
 
+    if (entradaValida) {
+        document.getElementById('entrada').value = display + entrada;
+    }
 }
 
+function validaEntrada(entrada, display) {
+    let entradaValida = true;
+
+    if ((entrada === '+' || entrada === '*' || entrada === '/') && display === '') {
+        entradaValida = false;
+    }
+
+    //operador de igualdade   a === b
+    //operador de diferença   a !== b
+
+    //saber se o display contem algum texto
+    //qual é o ultimo caracter do display e se ele é um operador matemático
+    // entrada tbm é um operador matemático
+
+    return entradaValida;
+}
 
 
 /**
